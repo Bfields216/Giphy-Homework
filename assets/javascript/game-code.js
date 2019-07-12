0569f61b6f242271e3d194627e2a563e944f8adc
Brandon Fields <brandonfields10@icloud.com>
2019-07-11 22:54:03 -0400

    working on code still, went back to original code, still having trouble gettin images to work and buttons to appear

--- assets/javascript/game-code.js
+++ assets/javascript/game-code.js
@@ -1,225 +1,253 @@
 // $(document).ready(function () {
 //     var nbaplayers = [
-//         "Lebron James",
-//         "Kobe Bryant",
-//         "Tim Duncan",
-//         "Lonzo Ball",
-//         "Chris Paul",
-//         "Kyrie Irving",
-//         "Scottie Pippen",
-//         "Blake Griffen",
-//         "Anthony Davis",
-//         "Zion Williams",
-//         "Tracy Mcgrady",
+//       "Lebron James",
+//       "Kobe Bryant",
+//       "Tim Duncan",
+//       "Lonzo Ball",
+//       "Chris Paul",
+//       "Kyrie Irving",
+//       "Scottie Pippen",
+//       "Blake Griffen",
+//       "Anthony Davis",
+//       "Zion Williams",
+//       "Tracy Mcgrady",
 //     ];
-
+  
 //     buttonDisplay()
 //     function buttonDisplay() {
-//         //created for loop that goes thru the nba players listed to search
-//         for (var i = 0; i < nbaplayers.length; i++) {
-//             $("#nba-button").append("<button type='button' class='btn btn-primary' value=' " + nbaplayers[i] + "'> " + nbaplayers[i] + " </button>");
-//             // event.preventDefult();
-//         }
-
-//   
-
-//
-
-
-//                 for (var i = 0; i < results.length; i++) {
-//                     // event.preventDefault();
-//                     var gifDiv = $('<div>');
-//                     gifDiv.addClass('col-4 gif-div');
-
-//                     var rated = $('<p>');
-//                     rated.addClass('rating');
-
-//                     rated.text("rated: " + results[i].rating);
-
-//                     var gifimage = $('<img>')
-
-//                     gifimage.attr("src", results[i].images.fixed_height_still.url);
-//                     gifimage.attr("gif-still", results[i].images.fixed_height_still.url);
-//                     gifimage.attr("gif-animate", results[i].images.fixed_height.url);
-//                     gifimage.attr("gif-state", "still")
-//                     gifimage.addClass('image-gif');
-
-//                     gifDiv.append(rated);
-//                     gifDiv.append(gifimage);
-
-//                     $('.gif-display').append(gifDiv);
-
-//                     function playGif () {
-//                         var state = $(this).attr('data-state');
-//                         if (state === 'still') {
-//                           $(this).attr('src', $(this).attr('data-animate'));
-//                           $(this).attr('data-state', 'animate');
-//                         }
-//                         else {
-//                           $(this).attr('src' , $(this).attr('data-still'));
-//                           $(this).attr('data-state', 'still');
-//                         }
-//                       }
-
-//                     $(document).on("click", ".animate-gif", playGif);
-//                 }
-//             })
-//             // renderButtons();
-//         });
+//       //created for loop that goes thru the nba players listed to search
+//     //   for (var i = 0; i < nbaplayers.length; i++) {
+//     //     $("#nba-button").append("<button type='button' class='btn btn-primary' value=' " + nbaplayers[i] + "'> " + nbaplayers[i] + " </button>");
+//     //     // event.preventDefult();
+//     //   }
+//       //document on click searches the URL for the user input or nba player listed
+//       $(document).on("click", 'button', function () {
+//         var gifName = $(this).attr("value");
+//         console.log(gifName);
+//         searchGif();
+//         // event.preventDefult(); 
+//         //apikey to search Giphy URL
+//         var apikey = "9iyLviGDMo6fB51jmQuzh8TYylX2LGFQ";
+//         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).html() + "&api_key=" + apikey + "&limit=10";
+//         console.log(queryURL)
+  
+  
+//         $('.info').empty();
+//         $.ajax({
+//           url: queryURL,
+//           method: "Get"
+//         }).then(function(response) {
+//           // If no information on nbaplayers is found, the alert the user
+//           if (response.pagination.total_count == 0) {
+//             alert('No Gifs came back');
+//             // var itemindex = nbaplayers.indexOf(topic);
+//             // otherwise display button
+//             if (itemindex > -1) {
+//             //   nbaplayers.splice(itemindex, 1);
+//             //   renderButtons();
+//               }
+//           }
+  
+//           var results = response.data;
+//           console.log(results);
+  
+  
+//           for (var i = 0; i < results.length; i++) {
+//             // event.preventDefault();
+//             var gifDiv = $('<div>');
+//             gifDiv.addClass('col-4 gif-div');
+  
+//             var rated = $('<p>');
+//             rated.addClass('rating');
+  
+//             rated.text("rated: " + results[i].rating.toUpperCase());
+  
+//             var gifimage = $('<img>')
+  
+//             gifimage.attr("src", results[i].images.fixed_height_still.url);
+//             gifimage.attr("gif-still", results[i].images.fixed_height_still.url);
+//             gifimage.attr("gif-animate", results[i].images.fixed_height.url);
+//             gifimage.attr("gif-state", "still")
+//             gifimage.addClass('image-gif');
+  
+//             gifDiv.append(rated);
+//             gifDiv.append(gifimage);
+  
+//             $('.gif-display').append(gifDiv);
+  
+  
+  
+  
+//           }
+//         })
+//         // renderButtons();
+//       });
+//       //function to display buttons
+  
 //     }
-//     // event.preventDefault();
-
+  
 //     function searchGif() {
-//         var nplayers = $(this).attr("data-name");
-//         // event.preventDefult();
-//         if (nplayers !== undefined) {
-//             var p = nplayers;
-//         }
-//         else {
-//             p = $("#add-gifs").val();
-//         }
-//         event.preventDefault();
+//       var nplayers = $(this).attr("data-name");
+//       // event.preventDefult();
+//       if (nplayers !== undefined) {
+//         var p = nplayers;
+//       }
+//       else {
+//         p = $("#add-gifs").val();
+//       }
+//       event.preventDefault();
+  
+  
+    // };
+    // function renderButtons() {
+    //   $('.buttons-view').empty();
+    //   for (var i = 0; i < nbaplayers.length; i++) {
+    //     var newbuttons = $('<button>');
+    //     newbuttons.addClass('topic btn btn-info');
+    //     newbuttons.attr('data-name', nbaplayers[i]);
+    //     newbuttons.text(nbaplayers[i]);
+    //     $('.buttons-view').append(newbuttons);
+    //   }
+    // }
+  
+    // function removeButton(){
+    //   $("#nba-view").empty();
+    //   var noplayers = $(this).attr('data-lane');
+    //   var playerindex = nbaplayers.indexOf(noplayers);
+    //   if (playerindex > -1){
+    //     nbaplayers.splice(playerindex, 1);
+    //     buttonDisplay();
+    //   }
+    // }
+    // // Function to play or still Gif images
+    // function playGif () {
+    //   var state = $(this).attr('data-state');
+    //   if (state === 'still') {
+    //     $(this).attr('src', $(this).attr('data-animate'));
+    //     $(this).attr('data-state', 'animate');
+    //   }
+    //   else {
+    //     $(this).attr('src' , $(this).attr('data-still'));
+    //     $(this).attr('data-state', 'still');
+    //   }
+    // }
+    // // // Click on the submit button to add a new hero button
+    // $("#nenba").on("click", function(event) {
+    //   event.preventDefault();
+    //   // capture input from the form
+    //   var uinput = $("#nba-input").val();
+    //   // check if topic exsits already
+    //   if (nbaplayers.toString().toLowerCase().indexOf(uinput.toLowerCase()) === -1) {
+    //     alert("Topic already exists");
+    //   }
+    //   else {
+    //     nbaplayers.push(uinput);
+    //     buttonDisplay();
+    //   }
+    // });
+  
+    // // Click on hero button to display Gifs and other info from API
+    // $(document).on("click", ".topic", buttonDisplay);
+    // // Click on the Gif image to animate or make it still
+    // $(document).on("click", ".animate-gif", playGif);
+    // // Double-click on any hero button to remove it from the array. Tried this for the first time.
+    // $(document).on("dblclick", ".topic", removeButton);
+  
+    // // Calling the renderButtons function to display the intial buttons
+    // buttonDisplay();
+  
+  
+  
+  
+//   });
+  
+$(document).ready(function () {
+    var nbaplayers = [
+        "Lebron James",
+        "Kobe Bryant",
+        "Tim Duncan",
+        "Lonzo Ball",
+        "Chris Paul",
+        "Kyrie Irving",
+        "Scottie Pippen",
+        "Blake Griffen",
+        "Anthony Davis",
+        "Zion Williams",
+        "Tracy Mcgrady",
+    ];
 
-
-//     };
-// });
-$(document).ready(function() {
-    // creating loop to go through nba players
-    var nbaplayers = ['Lebron James', 'Kobe Bryant', 'Allen Iverson', 'Chris Paul', 'Tracy Mcgrady', 'Robert Horry', 'Zion Williamson', 'TJ Ford', 'Kyrie Irving'];
-
-
-    //Function to display info on the nbaplayers by calling an API and retrieving the info 
-    function displayInfo(){
-      $('#nba-view').empty();
-      var topic = $(this).attr('data-name');
-      var apikey = "9iyLviGDMo6fB51jmQuzh8TYylX2LGFQ";
-     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).html() + "&api_key=" + apikey + "&limit=10";
-     console.log(queryURL)
-
-      // AJAX call to GET information 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .then(function(response) {
-        // If no information on nbaplayers is found, the alert the user
-        if (response.pagination.total_count == 0) {
-          alert('Sorry, there are no Gifs for this topic');
-          var itemindex = nbaplayers.indexOf(topic);
-          // otherwise display button
-          if (itemindex > -1) {
-            nbaplayers.splice(itemindex, 1);
-            renderButtons();
-            }
+    buttonDisplay()
+    function buttonDisplay() {
+        for (var i = 0; i < nbaplayers.length; i++) {
+            $("#nba-button").append("<button type='button' class='btn btn-primary' value=' " + nbaplayers[i] + "'> " + nbaplayers[i] + " </button>");
+            // event.preventDefult();
         }
-        
-        // Save response from API call (JSON) to a variable results
-        var results = response.data;
-        for (var j = 0; j < results.length; j++){
-          // Create new Div
-          var newTopicDiv = $("<div class='hero-name'>");
-          // Save responses from API into variables and add to DOM
-          // GIF Rating
-          var Rating = $('<p>').text('Rating: ' + results[j].rating.toUpperCase());
-          // GIF Title
-        //   var Title = $('<p>').text('Title: ' + results[j].title.toUpperCase());
-          // GIF URL
-          var gifURL = results[j].images.fixed_height_still.url;         
-          var gif = $('<img>');
-          gif.attr('src', gifURL);
-          gif.attr('data-still', results[j].images.fixed_height_still.url);
-          gif.attr('data-animate', results[j].images.fixed_height.url);
-          gif.attr('data-state', 'still');
-          gif.addClass ('animate-gif');
-          // Appending info 
-          newTopicDiv.append(Rating);
-        //   newTopicDiv.append(Title);
-          newTopicDiv.append(gif);
-           // Putting the saved info to new div
-          $('#nba-view').prepend(newTopicDiv);
-        } 
+        $(document).on("click", 'button', function () {
+            var gifName = $(this).attr("value");
+            console.log(gifName);
+            searchGif();
+            // event.preventDefult(); 
+            var apikey = "9iyLviGDMo6fB51jmQuzh8TYylX2LGFQ";
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +$(this).html() + "&api_key=" + apikey + "&limit=10";
+            console.log(queryURL)
 
-        // var rated = $('<p>');
-//                     rated.addClass('rating');
 
-//                     rated.text("rated: " + results[i].rating);
+            $('.info').empty();
+            $.ajax({
+                url: queryURL,
+                method: "Get"
+            }).then(function (response) {
+                $('.gif-display').empty();
 
-//                     var gifimage = $('<img>')
+                var results = response.data;
+                console.log(results);
 
-//                     gifimage.attr("src", results[i].images.fixed_height_still.url);
-//                     gifimage.attr("gif-still", results[i].images.fixed_height_still.url);
-//                     gifimage.attr("gif-animate", results[i].images.fixed_height.url);
-//                     gifimage.attr("gif-state", "still")
-//                     gifimage.addClass('image-gif');
 
-//                     gifDiv.append(rated);
-//                     gifDiv.append(gifimage);
-      });
-    };
+                for (var i = 0; i < results.length; i++) {
+                    // event.preventDefault();
+                    var gifDiv = $('<div>');
+                    gifDiv.addClass('col-4 gif-div');
+
+                    var rated = $('<p>');
+                    rated.addClass('rating');
+
+                    rated.text("rated: " + results[i].rating);
+
+                    var gifimage = $('<img>')
+
+                    gifimage.attr("src", results[i].images.fixed_height_still.url);
+                    gifimage.attr("gif-still", results[i].images.fixed_height_still.url);
+                    gifimage.attr("gif-animate", results[i].images.fixed_height.url);
+                    gifimage.attr("gif-state", "still")
+                    gifimage.addClass('image-gif');
+
+                    gifDiv.prepend(rated);
+                    gifDiv.prepend(gifimage);
+
+                    $('.gif-display').append(gifDiv);
+
+
+
+
+                }
+            })
+            // renderButtons();
+        });
+    }
+
+    function searchGif() {
+        var nplayers = $(this).attr("data-name");
+        // event.preventDefult();
+        if (nplayers !== undefined) {
+            var p = nplayers;
+        }
+        else {
+            p = $("#add-gifs").val();
+        }
+
+
     
-    // Function for displaying buttons
-    function renderButtons() {
-      // Deletes the movies prior to adding new movies
-      $('.buttons-view').empty();
-      // Loops through the array of nbaplayers to create buttons for all nbaplayers
-      for (var i = 0; i < nbaplayers.length; i++) {
-        var createButtons = $('<button>');
-        createButtons.addClass('topic btn btn-info');
-        createButtons.attr('data-name', nbaplayers[i]);
-        createButtons.text(nbaplayers[i]);
-        $('.buttons-view').append(createButtons);
-      }
-    }
-
-    // Function to remove buttons
-    function removeButton(){
-      $("#nba-view").empty();
-      var topic = $(this).attr('data-name');
-      var itemindex = nbaplayers.indexOf(topic);
-      if (itemindex > -1) {
-        nbaplayers.splice(itemindex, 1);
-        renderButtons();
-      }
-    }
-
-    // Function to play or still Gif images
-    function playGif () {
-      var state = $(this).attr('data-state');
-      if (state === 'still') {
-        $(this).attr('src', $(this).attr('data-animate'));
-        $(this).attr('data-state', 'animate');
-      }
-      else {
-        $(this).attr('src' , $(this).attr('data-still'));
-        $(this).attr('data-state', 'still');
-      }
-    }
-
-    ///EVENT LISTENERS aka CLICK EVENTS
-    // Click on the submit button to add a new hero button
-    $("#add-hero").on("click", function(event) {
-      event.preventDefault();
-      // capture input from the form
-      var hero = $("#nba-input").val().trim();
-      // check if topic exsits already
-      if (nbaplayers.toString().toLowerCase().indexOf(hero.toLowerCase()) != -1) {
-        alert("Topic already exists");
-      }
-      else {
-        nbaplayers.push(hero);
-        renderButtons();
-      }
-    });
-
-    // Click on hero button to display Gifs and other info from API
-    $(document).on("click", ".topic", displayInfo);
-    // Click on the Gif image to animate or make it still
-    $(document).on("click", ".animate-gif", playGif);
-    // Double-click on any hero button to remove it from the array. Tried this for the first time.
-    $(document).on("dblclick", ".topic", removeButton);
-
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+    };
 
 
-}); 
+});
